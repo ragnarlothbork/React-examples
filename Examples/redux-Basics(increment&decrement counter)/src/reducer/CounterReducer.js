503378f8964d929e66eec451b31fd8a625f6e500
RagnarLothbork <80914818+ragnarlothbork@users.noreply.github.com>
2021-06-15 16:33:34 +0530

    Add files via upload

---
+++ Examples/redux-Basics(increment&decrement counter)/src/reducer/CounterReducer.js
@@ -0,0 +1,11 @@
+const CounterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "ADD":
+      return state + action.payload;
+    case "SUB":
+      return state - action.payload;
+    default:
+      return state;
+  }
+};
+export default CounterReducer;
